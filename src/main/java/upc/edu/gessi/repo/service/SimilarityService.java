ad6679667295292b73bb69f45f0416ab22c6d598
Quim Motger <quim.motger@gmail.com>
2022-10-14 12:04:43 +0200

    add get top k similar apps with all documents (summary + descr + changelog)

--- src/main/java/upc/edu/gessi/repo/service/SimilarityService.java
+++ src/main/java/upc/edu/gessi/repo/service/SimilarityService.java
@@ -9,9 +9,9 @@ import upc.edu.gessi.repo.domain.DocumentType;
 import upc.edu.gessi.repo.domain.SimilarityAlgorithm;
 import upc.edu.gessi.repo.domain.SimilarityApp;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.print.Doc;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class SimilarityService {
@@ -32,10 +32,41 @@ public class SimilarityService {
     public Map<String, List<SimilarityApp>> getTopKSimilarApps(List<String> apps, int k, DocumentType documentType) {
         Map<String, List<SimilarityApp>> res = new HashMap<>();
         for (String app : apps) {
-            List<SimilarityApp> similarApps = graphDBService.getTopKSimilarApps(app, k, documentType);
+            List<SimilarityApp> similarApps = new ArrayList<>();
+            if (documentType.equals(DocumentType.ALL)) {
+                List<SimilarityApp> descriptionSimilarities = graphDBService.getTopKSimilarApps(app, k, DocumentType.DESCRIPTION);
+                List<SimilarityApp> summarySimilarities = graphDBService.getTopKSimilarApps(app, k, DocumentType.SUMMARY);
+                List<SimilarityApp> changelogSimilarities = graphDBService.getTopKSimilarApps(app, k, DocumentType.CHANGELOG);
+
+                similarApps = descriptionSimilarities;
+                mergeSimilarities(similarApps, summarySimilarities);
+                mergeSimilarities(similarApps, changelogSimilarities);
+                similarApps = similarApps.stream().sorted(Comparator.comparingDouble(SimilarityApp::getScore).reversed())
+                        .collect(Collectors.toList()).subList(0, k);
+
+            } else {
+                similarApps = graphDBService.getTopKSimilarApps(app, k, documentType);
+            }
             res.put("https://schema.org/MobileApplication/" + app, similarApps);
         }
         return res;
+    }
+
+    private void mergeSimilarities(List<SimilarityApp> similarApps, List<SimilarityApp> summarySimilarities) {
+        for (SimilarityApp app1 : summarySimilarities) {
+            SimilarityApp foundApp = null;
+            double score = 0.;
+            for (SimilarityApp app2 : similarApps) {
+                if (app1.getDocumentID().equals(app2.getDocumentID())) {
+                    foundApp = app2;
+                    score = app1.getScore();
+                }
+            }
+            if (foundApp != null) foundApp.setScore(foundApp.getScore() + score);
+            else {
+                similarApps.add(app1);
+            }
+        }
     }
 
     public void computeFeatureSimilarity() {
