0e0682595339ba8ef0277c3f007790b1405704e6
mtiessler <max.tiessler@upc.edu>
2024-05-29 15:00:26 +0200

    srj to json script

---
+++ src/main/scripts/srj_to_json_list.py
@@ -0,0 +1,34 @@
+import ijson
+import json
+import re
+from rdflib import URIRef
+
+SRJ_FILE = 'definedTerms.srj'
+RESULT_FILE = 'definedTerms.json'
+
+
+def map_srj_to_json():
+    definedTerms = []
+    with open(SRJ_FILE, 'r', encoding="utf8") as srj_file:
+        for binding in ijson.items(srj_file, "results.bindings.item"):
+            uri = str(URIRef(binding['s']['value']))
+            if "DefinedTerm/" in uri:
+                term = uri.split("DefinedTerm/")[1]
+                if term not in definedTerms:
+                    definedTerms.append(term)
+    return definedTerms
+
+
+def srj_to_json():
+    defined_terms = map_srj_to_json()
+    return json.dumps(defined_terms, indent=4)
+
+
+def main():
+    json_data = srj_to_json()
+    with open(RESULT_FILE, 'w', encoding="utf8") as result_file:
+        result_file.write(json_data)
+
+
+if __name__ == "__main__":
+    main()
