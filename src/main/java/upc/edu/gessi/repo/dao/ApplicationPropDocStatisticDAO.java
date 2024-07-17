77840127d63a1203792c3fecacd8de343989905f
mtiessler <max.tiessler@upc.edu>
2024-07-17 19:33:23 +0200

    added better daos

--- src/main/java/upc/edu/gessi/repo/dao/ApplicationPropDocStatisticDAO.java
+++ src/main/java/upc/edu/gessi/repo/dao/ApplicationPropDocStatisticDAO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,4 +14,6 @@ public class ApplicationPropDocStatisticDAO {
     private Integer reviewFeaturesCount;
     private Integer summaryFeaturesCount;
     private Integer descriptionFeaturesCount;
+    private Date firstDate;
+    private Date endDate;
 }
