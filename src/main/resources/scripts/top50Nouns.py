233aeda8f0210ac6640627c83ec753569924d19c
mtiessler <max.tiessler@upc.edu>
2024-07-25 16:47:11 +0200

    ongoing new algorithm

--- src/main/resources/scripts/top50Nouns.py
+++ src/main/resources/scripts/top50Nouns.py
@@ -1,15 +1,16 @@
 import sys
 import json
 import re
+import logging
 from collections import Counter
 import nltk
+from argparse import ArgumentParser
 
 try:
     nltk.data.find('tokenizers/punkt')
     nltk.data.find('taggers/averaged_perceptron_tagger')
 except LookupError:
     import subprocess
-
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'nltk'])
     nltk.download('punkt')
     nltk.download('averaged_perceptron_tagger')
@@ -27,44 +28,62 @@ def split_into_list(text):
     return text.split()
 
 
-def extract_nouns_from_text(text):
-    cleaned_feature = clean_text(text['feature'])
-    feature_words = []
-    for word in nltk.word_tokenize(cleaned_feature):
-        feature_words.extend(camel_case_split(word))
 
-    cleaned_sentence = clean_text(text['sentence'])
-    sentence_words = []
-    for word in nltk.word_tokenize(cleaned_sentence):
-        sentence_words.append(word)
-
-    tagged_feature = nltk.pos_tag(feature_words)
-    tagged_sentence = nltk.pos_tag(sentence_words)
-
-    feature_nouns = {word for word, pos in tagged_feature if pos.startswith('NN')}
-    sentence_nouns = {word for word, pos in tagged_sentence if pos.startswith('NN')}
-
-    common_nouns = feature_nouns.intersection(sentence_nouns)
-    return common_nouns
-
-
-def main():
+def extraction(pos_tag_prefix):
+    # 1) Get Feature and sentence from input
     input_text = sys.stdin.read()
     text_list = json.loads(input_text)
 
-    noun_counter = Counter()
 
     for text in text_list:
-        common_nouns = extract_nouns_from_text(text)
-        for noun in common_nouns:
-            noun_counter[noun] += 1
+        # 2) Clean feature and sentence
+        cleaned_feature = clean_text(text['feature'])
+        feature_words = []
+        for word in nltk.word_tokenize(cleaned_feature):
+            feature_words.extend(camel_case_split(word))
 
-    top_50_nouns = noun_counter.most_common(50)
+        cleaned_sentence = clean_text(text['sentence'])
+        sentence_words = []
+        for word in nltk.word_tokenize(cleaned_sentence):
+            sentence_words.append(word)
+        # 3) Tag sentence
+        tagged_sentence = nltk.pos_tag(sentence_words)
 
-    top_50_nouns_list = [{'term': noun, 'frequency': freq} for noun, freq in top_50_nouns]
+        # 4) Initialize counter and matched positions
+        word_counter = Counter()
+        matched_words_positions = []
+        # 4) Iterate per feature word
+        for feature_word in feature_words:
+            if feature_word in sentence_words:
+                for i, sentence_word in enumerate(sentence_words):
+                    if sentence_word == feature_word:
+                        tag = tagged_sentence[i][1]
+                        tagged_word = tagged_sentence[i][0]
 
-    print(json.dumps(top_50_nouns_list, indent=2))
+                        # matched_words_with_pos[sentence_word] = i
+                        break
+            else:
+                logging.info(f'Word {feature_word} from feature not found in sentence')
+                return []
+
+
+    top_50_words = word_counter.most_common(50)
+
+    top_50_words_list = [{'term': word, 'frequency': freq} for word, freq in top_50_words]
+    logging.info(f'Result')
+    print(json.dumps(top_50_words_list, indent=2))
+    print(json.dumps(matched_words_positions, indent=2))
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(description="Extract NN or VV from text")
+    parser.add_argument('--tag', choices=['NN', 'VV'], required=True, help="Specify 'NN' for noun extraction or 'VV' for verb extraction")
+    args = parser.parse_args()
+
+    logging.basicConfig(filename='analysis.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+    logging.info('Starting analysis')
+    logging.info(f'----------------------------------')
+    pos_tag_prefix = 'NN' if args.tag == 'NN' else 'VB'
+    extraction(pos_tag_prefix)
+    logging.info(f'----------------------------------')
+    logging.info('Analysis completed')
