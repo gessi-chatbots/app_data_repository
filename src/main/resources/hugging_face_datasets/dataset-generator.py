06926d5e7adb4aef22e194d5178254c5979d1ec8
mtiessler <max.tiessler@upc.edu>
2024-08-12 10:28:49 +0200

    ongoing

---
+++ src/main/resources/hugging_face_datasets/dataset-generator.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def read_csv_file():
+    return None
+
+def merge_csv_files():
+    return None
+
+def generate_datasets():
+    return None
+
+def main():
+
+    return None
+
+
+if __name__ == '__main__':
+    main()
